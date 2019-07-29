1b7359f7fa56a473ae4c5cc7f52f3b9a1f90ba68
Dmitry Marchuk <dimonoso96@gmail.com>
2019-07-29 16:59:00 +0300

    addded camera configuration

---
+++ Assets/Scripts/Views/CameraSettings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Tetris.Views
+{
+    public class CameraSettings : MonoBehaviour
+    {
+        [SerializeField]
+        private float _baseScreenHeight;
+        [SerializeField]
+        private float _baseScreenWidth;
+
+        private Camera _camera;
+
+        private void Start()
+        {
+            _camera = GetComponent<Camera>();
+            var baseOrthographicSize = _camera.orthographicSize;
+            var currentHeight = (float)_camera.pixelHeight;
+            var currentWidth = (float)_camera.pixelWidth;
+
+            _camera.orthographicSize = baseOrthographicSize
+                                       / (currentWidth / currentHeight)
+                                       * (_baseScreenWidth / _baseScreenHeight);
+        }
+    }
+}
